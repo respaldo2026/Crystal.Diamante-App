8fc05833c4be049b1b15be1b3de008ee6c918f25
German <newsocialmedia2.0@gmail.com>
2026-02-24 17:34:48 -0500

    feat: actualizar consulta de sesiones para filtrar por rango de fechas

--- src/app/estudiantes/show/[id]/page.tsx
+++ src/app/estudiantes/show/[id]/page.tsx
@@ -258,7 +258,7 @@ export default function StudentDetailView() {
             .in("curso_id", cursoIds);
 
           const { data: sesionesData } = await (fechaMin && fechaMax
-            ? sesionesQuery.gte("fecha", fechaMin).lte("fecha", fechaMax)
+            ? sesionesQuery.filter("fecha", "cs", `[${fechaMin},${fechaMax}]`)
             : sesionesQuery);
 
           const temaPorCursoFecha = new Map<string, string>();
